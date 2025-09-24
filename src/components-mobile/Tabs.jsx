defed0c6fbd947c4a7b482628b324d7fb2772d39
Katherine Sick <katysick11@gmail.com>
2025-09-24 16:43:35 -0500

    added mobile experience section

--- src/components-mobile/Tabs.jsx
+++ src/components-mobile/Tabs.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import ProjectsMobile from './Projects';
 import Education from './Education';
-// import Experience from './Experience';
+import Experience from './Experience';
 
 export default function Tabs() {
   const [selectedTab, setSelectedTab] = useState('projects');
@@ -20,17 +20,19 @@ export default function Tabs() {
           display: 'flex',
           justifyContent: 'space-around',
           marginBottom: '12px',
+          marginRight: '8px',
+          paddingTop: '12px',
           flexWrap: 'wrap'
         }}>
           <button className="tab" aria-selected={selectedTab === 'projects'} onClick={() => setSelectedTab('projects')}>Projects</button>
           <button className="tab" aria-selected={selectedTab === 'education'} onClick={() => setSelectedTab('education')}>Education</button>
-          {/* <button className="tab" aria-selected={selectedTab === 'experience'} onClick={() => setSelectedTab('experience')}>Experience</button> */}
+          <button className="tab" aria-selected={selectedTab === 'experience'} onClick={() => setSelectedTab('experience')}>Experience</button>
         </div>
 
         <div className="tabpanels-mobile" style={{ textAlign: 'left' }}>
           {selectedTab === 'projects' && <ProjectsMobile />}
           {selectedTab === 'education' && <Education />}
-          {/* {selectedTab === 'experience' && <Experience />} */}
+          {selectedTab === 'experience' && <Experience />}
         </div>
       </div>
     </div>
